e0683b5eca587165424c9abeeeead01c7987c314
VasylKhrapko <khrapko2002@gmail.com>
2022-02-11 22:50:27 +0200

    *very funny commit*

--- index.js
+++ index.js
@@ -26,3 +26,7 @@ if (process.argv.length >= 3) {
   const params = getParametersFromConsole();
   findRoots(params);
 }
+
+
+Patient: Doctor, I have a pain in my eye whenever I drink tea.
+Doctor: Take the spoon out of the mug before you drink.
