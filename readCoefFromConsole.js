cbc1a68186c81b8dab8adda941dcd51e4cc0b896
VasylKhrapko <khrapko2002@gmail.com>
2022-02-11 22:14:37 +0200

    Created a function to get parameters from the console
    Created a function getCoefFromConsole, that receives the parameter value
    entered by the user from the console, and check if it matches the
    expected format (real number). Also  created function
    getParametersFromConsole, that receives 3 parameters (which we need
    to solve the quadratic equation in the general case) using
    getCoefFromConsole function, and returns them as a array.
    The implementation of two different functions is made for decomposition,
    and for a better understanding of the code.
    A dependency readline-sync was used to read input from the console,
    specifically this was chosen for its simplicity, which suits
    our program. Also added a .gitignor file to hide node-modules folder.
    
    readline-sync: https://www.npmjs.com/package/readline-sync

---
+++ readCoefFromConsole.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const readlineSync = require('readline-sync');
+
+const getCoefFromConsole = coefName => {
+  while (true) {
+    const coef = readlineSync.question(`${coefName} = `);
+    const numCoef = +coef;
+    if (isNaN(numCoef) || coef === '') {
+      console.log(`Error. Expected a valid real number, got ${coef} instead`);
+    } else return numCoef;
+  }
+};
+
+const getParametersFromConsole = (nameA = 'a', nameB = 'b', nameC = 'c') => {
+  const coefArr = [nameA, nameB, nameC];
+  const paramsArr = [];
+
+  for (const coef of coefArr) {
+    const param = getCoefFromConsole(coef);
+    paramsArr.push(param);
+  }
+  return paramsArr;
+};
